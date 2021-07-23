c0d94abe51c2e7491a08b064b14dfac0660c00f8
HansUniVie <hans.kainz@univie.ac.at>
2021-07-23 12:33:51 +0200

    script to calculate GAS length without OpenSim (but based on the 2392 OpenSim mode)

---
+++ calculateGASlengthFromC3D.m
@@ -0,0 +1,28 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% hans.kainz@univie.ac.at
+% July 2021
+% this code calculates GAST length based on the knee and ankle angles from
+% a c3d file
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear all; clc; close all;
+home ='C:\Users\Hans\Documents\WORK_KULeuven\MATLABcodes\Gastroc_length_function\';
+cd(home)
+% load C3D file data and get knee and ankle angles
+exampleC3D = 'g0h1_2 41.c3d';
+dataC3D = btk_loadc3d(exampleC3D); % load c3d file
+
+val_k = dataC3D.angles.RKneeAngles(:,1); % sagittal knee angle
+val_a = dataC3D.angles.RAnkleAngles(:,1); % sagittal ankle angle
+
+% calculate GAST length
+medGAST = gastlength(val_k,val_a,1);
+latGAST = gastlength(val_k,val_a,2);
+    
+ 
+
+
+
+
+
+
